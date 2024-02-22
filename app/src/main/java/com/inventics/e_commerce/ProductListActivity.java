827952510c4534be921bf107b15f63b0d92d0a48
Shivansh Kumar <codes.stalker@gmail.com>
2024-02-22 16:58:39 +0530

    Initial Commit

---
+++ app/src/main/java/com/inventics/e_commerce/ProductListActivity.java
@@ -0,0 +1,16 @@
+package com.inventics.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+public class ProductListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(R.layout.activity_productlist);
+    }
+}
---
+++ app/src/main/java/com/inventics/e_commerce/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.inventics.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(R.layout.activity_splash);
+
+        FirebaseDatabase.getInstance().getReference().child("shivanshCheck").setValue("shivans is connected");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, ProductListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+
+    }
+}
