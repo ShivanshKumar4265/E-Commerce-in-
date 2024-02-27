4a90e6edefc7a71460f0a95a619c9253a532ba2e
Shivansh Kumar <codes.stalker@gmail.com>
2024-02-27 13:22:51 +0530

    Integrated ProductList Frag

--- app/src/main/java/com/inventics/e_commerce/activities/MainActivity.java
+++ app/src/main/java/com/inventics/e_commerce/activities/MainActivity.java
@@ -1,16 +1,78 @@
 package com.inventics.e_commerce.activities;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.inventics.e_commerce.R;
+import com.inventics.e_commerce.fragments.AccountFragment;
+import com.inventics.e_commerce.fragments.CartFragment;
+import com.inventics.e_commerce.fragments.CategoryFragment;
+import com.inventics.e_commerce.fragments.ProductListFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(Html.fromHtml("<font color='#FFFFFF'>Product List</font>"));
+        }
+
+        findingAllTheViews();
+
+        setFragmentOnFirstLaunch();
+
+        settingOnClickListener();
+    }
+
+    private void setFragmentOnFirstLaunch() {
+        replaceFragment(new ProductListFragment());
+    }
+
+    private void settingOnClickListener() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.homeFragment) {
+                    replaceFragment(new ProductListFragment());
+                } else if (item.getItemId() == R.id.cartFragment) {
+                    replaceFragment(new CartFragment());
+                } else if (item.getItemId() == R.id.accountFragment) {
+                    replaceFragment(new CategoryFragment());
+                } else if (item.getItemId() == R.id.categoryFragment) {
+                    replaceFragment(new AccountFragment());
+                }
+                return true;
+            }
+        });
+    }
+
+    private void findingAllTheViews() {
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mainActivityFrameLayout,fragment);
+        fragmentTransaction.commit();
+
     }
 }
--- app/src/main/java/com/inventics/e_commerce/fragments/ProductListFragment.java
+++ app/src/main/java/com/inventics/e_commerce/fragments/ProductListFragment.java
@@ -1,65 +1,126 @@
 package com.inventics.e_commerce.fragments;
+//
+//import android.os.Bundle;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import androidx.fragment.app.Fragment;
+//
+//import com.inventics.e_commerce.R;
+//public class ProductListFragment extends Fragment {
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_product_list, container, false);
+//    }
+//}
 
+
+//package com.inventics.e_commerce.fragments;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.inventics.e_commerce.R;
+import com.inventics.e_commerce.activities.AddProductActivity;
+import com.inventics.e_commerce.adapters.ProductDataAdapter;
+import com.inventics.e_commerce.modal.Product;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProductListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class ProductListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProductListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProductListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProductListFragment newInstance(String param1, String param2) {
-        ProductListFragment fragment = new ProductListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private RecyclerView productRecyclerView;
+    private ArrayList<Product> data = new ArrayList<>();
+    private ProductDataAdapter mAdapter;
+    private FloatingActionButton fab_addProduct;
+    private DatabaseReference databaseRef;
+    private ProgressBar progressBar;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_product_list, container, false);
+
+        fab_addProduct = view.findViewById(R.id.fab_addProduct);
+        productRecyclerView = view.findViewById(R.id.rv_productList);
+        progressBar = view.findViewById(R.id.progressBar_productList);
+
+        settingUpRecyclerView(productRecyclerView);
+
+        fab_addProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentToProductDescriptionActivity();
+            }
+        });
+
+        gettingProductData();
+
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_list, container, false);
+    private void intentToProductDescriptionActivity() {
+        startActivity(new Intent(requireActivity(), AddProductActivity.class));
+    }
+
+    private void settingUpRecyclerView(RecyclerView productRecyclerView) {
+        productRecyclerView.setHasFixedSize(true);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(requireContext(), 2);
+        productRecyclerView.setLayoutManager(gridLayoutManager);
+    }
+
+    private void gettingProductData() {
+        databaseRef = FirebaseDatabase.getInstance().getReference().child("Product");
+
+        databaseRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (isAdded() && getActivity() != null) {
+                    // Your UI-related operations here
+                    progressBar.setVisibility(View.VISIBLE);
+                    data.clear();
+                    Log.i("Snapshot", snapshot.toString());
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        Product productData = dataSnapshot.getValue(Product.class);
+                        if (productData != null) {
+                            productData.setKey(dataSnapshot.getKey());
+                        }
+                        Log.i("productDataTitle", productData.getTitle());
+                        Log.i("productDataDescription", productData.getDescription());
+                        Log.i("productDataImage", productData.getImage());
+                        data.add(productData);
+                    }
+                    mAdapter = new ProductDataAdapter(data, requireContext());
+                    progressBar.setVisibility(View.INVISIBLE);
+                    productRecyclerView.setAdapter(mAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+
+        });
     }
 }
+
+
