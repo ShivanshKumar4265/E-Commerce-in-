f29ec48e0b1fb977aa00d9e32dc87b752e150344
Shivansh Kumar <codes.stalker@gmail.com>
2024-02-27 10:05:48 +0530

    Added Read More/Less feature

--- app/src/main/java/com/inventics/e_commerce/activities/ProductDescriptionActivity.java
+++ app/src/main/java/com/inventics/e_commerce/activities/ProductDescriptionActivity.java
@@ -4,7 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
@@ -22,18 +27,20 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.inventics.e_commerce.R;
+import com.inventics.e_commerce.java_class.MySpannable;
 import com.inventics.e_commerce.modal.Product;
 
 
 public class ProductDescriptionActivity extends AppCompatActivity {
-    ImageView productImage1,productImage2,productImage3;
+    ImageView productImage1, productImage2, productImage3;
     ViewFlipper viewFlipper;
-    TextView productCategory, productTitle,productDescription,productPrice,productRateCount;
+    TextView productCategory, productTitle, productDescription, productPrice, productRateCount;
     RatingBar productRating;
     Button deleteData, updateData;
     String p_id;
 
     SharedPreferences myPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,7 +68,6 @@ public class ProductDescriptionActivity extends AppCompatActivity {
         });
 
 
-
     }
 
     private void updateProductData() {
@@ -87,8 +93,8 @@ public class ProductDescriptionActivity extends AppCompatActivity {
 
     private void gettingProductKeyViaSharedPreference() {
         myPreferences = PreferenceManager.getDefaultSharedPreferences(ProductDescriptionActivity.this);
-        p_id = myPreferences.getString("p_id","");
-        Toast.makeText(ProductDescriptionActivity.this, "Pid="+p_id, Toast.LENGTH_SHORT).show();
+        p_id = myPreferences.getString("p_id", "");
+        Toast.makeText(ProductDescriptionActivity.this, "Pid=" + p_id, Toast.LENGTH_SHORT).show();
     }
 
     private void deleteProductData() {
@@ -101,16 +107,16 @@ public class ProductDescriptionActivity extends AppCompatActivity {
                 .addOnSuccessListener(aVoid -> {
                     // Deletion successful
                     Toast.makeText(this, "Delete Successfully", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(ProductDescriptionActivity.this,ProductListActivity.class));
+                    startActivity(new Intent(ProductDescriptionActivity.this, ProductListActivity.class));
                 })
                 .addOnFailureListener(e -> {
                     // Deletion failed
-                    Toast.makeText(this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 });
     }
 
     private void getDescriptionOfTheProduct() {
-        FirebaseDatabase database  = FirebaseDatabase.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = database.getReference().child("Product");
         databaseReference.child(p_id).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -120,10 +126,10 @@ public class ProductDescriptionActivity extends AppCompatActivity {
 
                     // Check if the product object is not null before accessing its properties
                     if (product != null) {
-                        productTitle.setText(product.getTitle() != null ? product.getTitle().toString() : "");
-                        productDescription.setText(product.getDescription() != null ? product.getDescription().toString() : "");
+                        productTitle.setText(product.getTitle() != null ? product.getTitle() : "");
+                        productDescription.setText(product.getDescription() != null ? product.getDescription() : "");
                         productPrice.setText("INR " + (product.getPrice() != null ? product.getPrice() : ""));
-                        productCategory.setText(product.getCategory() != null ? product.getCategory().toString() : "");
+                        productCategory.setText(product.getCategory() != null ? product.getCategory() : "");
                         productRateCount.setText((product.getRating() != null ? String.valueOf(product.getRating().getCount()) : "") + " Reviews");
                         productRating.setRating(product.getRating() != null ? product.getRating().getRate() : 0);
                         Glide.with(ProductDescriptionActivity.this)
@@ -139,6 +145,7 @@ public class ProductDescriptionActivity extends AppCompatActivity {
                                 .into(productImage3);
 
                         viewFlipper.startFlipping();
+                        makeTextViewResizable(productDescription,2,"...Read more",true);
                     } else {
                         // Handle the case where the product object is null
                         Toast.makeText(ProductDescriptionActivity.this, "Product not found", Toast.LENGTH_SHORT).show();
@@ -148,10 +155,93 @@ public class ProductDescriptionActivity extends AppCompatActivity {
                     Toast.makeText(ProductDescriptionActivity.this, "Product not found", Toast.LENGTH_SHORT).show();
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
 
     }
+
+
+    public static void makeTextViewResizable(final TextView tv, final int maxLine, final String expandText, final boolean viewMore) {
+
+        if (tv.getTag() == null) {
+            tv.setTag(tv.getText());
+        }
+
+
+        ViewTreeObserver vto = tv.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onGlobalLayout() {
+
+                ViewTreeObserver obs = tv.getViewTreeObserver();
+                obs.removeGlobalOnLayoutListener(this);
+                if (maxLine == 0) {
+                    int lineEndIndex = tv.getLayout().getLineEnd(0);
+                    String text = tv.getText().subSequence(0, lineEndIndex - expandText.length() + 1) + " " + expandText;
+                    tv.setText(text);
+                    tv.setMovementMethod(LinkMovementMethod.getInstance());
+                    tv.setText(
+                            addClickablePartTextViewResizable(Html.fromHtml(tv.getText().toString()), tv, maxLine, expandText,
+                                    viewMore), TextView.BufferType.SPANNABLE);
+                } else if (maxLine > 0 && tv.getLineCount() >= maxLine) {
+                    int lineEndIndex = tv.getLayout().getLineEnd(maxLine - 1);
+                    String text = tv.getText().subSequence(0, lineEndIndex - expandText.length() + 1) + " " + expandText;
+                    tv.setText(text);
+                    tv.setMovementMethod(LinkMovementMethod.getInstance());
+                    tv.setText(
+                            addClickablePartTextViewResizable(Html.fromHtml(tv.getText().toString()), tv, maxLine, expandText,
+                                    viewMore), TextView.BufferType.SPANNABLE);
+                } else {
+                    int lineEndIndex = tv.getLayout().getLineEnd(tv.getLayout().getLineCount() - 1);
+                    String text = tv.getText().subSequence(0, lineEndIndex) + " " + expandText;
+                    tv.setText(text);
+                    tv.setMovementMethod(LinkMovementMethod.getInstance());
+                    tv.setText(
+                            addClickablePartTextViewResizable(Html.fromHtml(tv.getText().toString()), tv, lineEndIndex, expandText,
+                                    viewMore), TextView.BufferType.SPANNABLE);
+                }
+            }
+        });
+
+    }
+
+    private static SpannableStringBuilder addClickablePartTextViewResizable(final Spanned strSpanned,
+                                                                            final TextView tv,
+                                                                            final int maxLine,
+                                                                            final String spanableText,
+                                                                            final boolean viewMore) {
+        String str = strSpanned.toString();
+        SpannableStringBuilder ssb = new SpannableStringBuilder(strSpanned);
+
+        if (str.contains(spanableText)) {
+
+
+            ssb.setSpan(new MySpannable(false) {
+                @Override
+                public void onClick(View widget) {
+                    if (viewMore) {
+                        tv.setLayoutParams(tv.getLayoutParams());
+                        tv.setText(tv.getTag().toString(), TextView.BufferType.SPANNABLE);
+                        tv.invalidate();
+                        makeTextViewResizable(tv, -1, "Read Less", false);
+                    } else {
+                        tv.setLayoutParams(tv.getLayoutParams());
+                        tv.setText(tv.getTag().toString(), TextView.BufferType.SPANNABLE);
+                        tv.invalidate();
+                        makeTextViewResizable(tv, 3, "... Read More", true);
+                    }
+                }
+            }, str.indexOf(spanableText), str.indexOf(spanableText) + spanableText.length(), 0);
+
+        }
+        return ssb;
+
+    }
+
+
 }
